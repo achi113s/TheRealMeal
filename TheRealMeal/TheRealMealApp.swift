ce611906a40a94347a76d3ddfbdef4f6333990b4
achi113s <latourgiorgio@gmail.com>
2023-07-03 12:12:26 -0400

    first commit

---
+++ TheRealMeal/TheRealMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheRealMealApp.swift
+//  TheRealMeal
+//
+//  Created by Giorgio Latour on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TheRealMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheRealMealUITests/TheRealMealUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TheRealMealUITestsLaunchTests.swift
+//  TheRealMealUITests
+//
+//  Created by Giorgio Latour on 7/3/23.
+//
+
+import XCTest
+
+final class TheRealMealUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
